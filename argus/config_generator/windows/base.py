948be16d98bbda87e782a8a1e7bb8046cba4aeec
Daniel Țoncu <dtoncu@cloudbasesolutions.com>
2016-10-17 17:24:16 +0300

    Fix Travis jobs
    Fixes any failing unit tests and also fixes any other existing coding
    style issues.
    
    Co-Authored-By: Stefan Caraiman <scaraiman@cloudbasesolutions.com>

--- argus/config_generator/windows/base.py
+++ argus/config_generator/windows/base.py
@@ -12,13 +12,18 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
 import abc
 import ntpath
-import StringIO
+import six
+
+try:
+    import StringIO
+except ImportError:
+    import io as StringIO
 
 from argus.config_generator import base
 from argus import util
-import six
 
 LOG = util.get_logger()
 
@@ -59,7 +64,16 @@ class BaseWindowsConfig(base.BaseConfig):
         base_conf = StringIO.StringIO(
             util.get_resource(config_name))
         conf = six.moves.configparser.ConfigParser()
-        conf.readfp(base_conf)
+
+        # NOTE(dtoncu): `readfp` is deprecated since Python 3.2,
+        # but it was replaced with `read_file`.
+        if six.PY2:
+            # pylint: disable=deprecated-method
+            conf.readfp(base_conf)
+        else:
+            # pylint: disable=no-member
+            conf.read_file(base_conf)
+
         return conf
 
     @abc.abstractmethod
